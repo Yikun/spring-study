00b8d870e55967ba408ba4c8cfb5ccc264b68700
Jiang Yikun <yikunkero@gmail.com>
2015-06-03 23:29:51 +0800

    Add: audience using aop feature.

---
+++ src/main/java/com/springstudy/talentshow/Audience.java
@@ -0,0 +1,21 @@
+package com.springstudy.talentshow;
+
+public class Audience {
+	public Audience() {}
+	
+	public void takeSeats() {
+		System.out.println("[Audience] take seats.");
+	}
+	
+	public void turnOffCellPhones() {
+		System.out.println("[Audience] turn off cell phones.");
+	}
+	
+	public void applaud() {
+		System.out.println("[Audience] Wow, Amazing! Applaud.");
+	}
+	
+	public void demandRefund() {
+		System.out.println("[Audience] Xu xu xu, yu yu yu~");
+	}
+}
